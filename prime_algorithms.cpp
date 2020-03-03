9f72f22d1d5dfa5f9919864336a65ceaac166e99
plerros <48536508+plerros@users.noreply.github.com>
2020-03-03 11:21:35 +0200

    Update prime_algorithms.cpp

--- prime_algorithms.cpp
+++ prime_algorithms.cpp
@@ -3,18 +3,8 @@
 #include <stdint.h>
 #include <iostream>
 
-#define MINIMUM	0
-#define MAXIMUM 100000000
-
-uint64_t normalizer(uint64_t current){
-	if(current == 2)
-		return 1;
-	for(uint32_t i = 2; current % i != 0; i++){
-		if(i*i > current)
-			return 1;
-	}
-	return 0;
-}
+#define MINIMUM 10000000
+#define MAXIMUM 20000000
 
 void print_stats(std::string algorithm, uint64_t counter, clock_t time){
 	algorithm += ':';
@@ -30,36 +20,46 @@ void deterministic(uint64_t current, uint64_t range){
 	clock_t time = clock();
 
 	if(range >= 2){
+		bool exit_flag = false;
 		//preperation
-		if(current < 2)
+		if(current <= 2 && !exit_flag){
 			current = 2;
-		while(current < 5){
-			counter += normalizer(current);
+			counter++;
 			if(current < range)
 				current++;
-			else{
-				time = clock() - time;
-				print_stats("Deterministic", counter, time);
-				return;
-			}
+			else
+				exit_flag = true;
 		}
-		if(current % 2 == 0 && current < range)
-			current ++;
+		while(current < 5 && !exit_flag){
+			for(uint32_t i = 2; current % i != 0; i++){
+				if(i*i > current){
+					counter++;
+					break;
+				}
+			}
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
+		if(current % 2 == 0 && !exit_flag){
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
 		//deterministic algorithm
-		while(1){
+		while(!exit_flag){
 			for(uint32_t i = 3; current % i != 0; i += 2){
 				if(i*i > current){
-					counter ++;
+					counter++;
 					break;
 				}
 			}
 			if(current +1 < range)
 				current += 2;
-			else{
-				time = clock() - time;
-				print_stats("Deterministic", counter, time);
-				return;
-			}
+			else
+				exit_flag = true;
 		}
 	}
 	time = clock() - time;
@@ -77,15 +77,23 @@ void atkin(uint64_t minimum, uint64_t range){
 	if(minimum > range)
 		return;
 
-	uint64_t current = 2, counter = 0;
+	uint64_t current = 0, counter = 0;
 	node *primearray = new node(2);
 	node *last(primearray);
 	clock_t time = clock();
 
 	if(range >= 2){
-		if(minimum <= 2)
-			counter = 1;
-		while(current < 5){
+		bool exit_flag = false;
+		if(current <= 2 && !exit_flag){
+			current = 2;
+			if(minimum <= 2)
+				counter++;
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
+		while(current < 5 && !exit_flag){
 			for(node* tempnode = primearray;current % tempnode->prime != 0; tempnode = tempnode->next){
 				if(current < tempnode->prime * tempnode->prime){
 					last->next = new node(current);
@@ -96,19 +104,10 @@ void atkin(uint64_t minimum, uint64_t range){
 			}
 			if(current < range)
 				current++;
-			else{
-				time = clock() - time;
-				print_stats("Sieve of Atkin", counter, time);
-				for(struct node *tempnode; primearray != last;){
-					tempnode = primearray;
-					primearray = primearray->next;
-					delete tempnode;
-				}
-				delete primearray;
-				return;
-			}
+			else
+				exit_flag = true;
 		}
-		while(current < minimum){
+		while(current < minimum && !exit_flag){
 			for(node* tempnode = primearray;current % tempnode->prime != 0; tempnode = tempnode->next){
 				if(current < tempnode->prime * tempnode->prime){
 					(last)->next = new node(current);
@@ -118,42 +117,28 @@ void atkin(uint64_t minimum, uint64_t range){
 			}
 			if(current < range)
 				current++;
-			else{
-				time = clock() - time;
-				print_stats("Sieve of Atkin", counter, time);
-				for(struct node *tempnode; primearray != last;){
-					tempnode = primearray;
-					primearray = primearray->next;
-					delete tempnode;
-				}
-				delete primearray;
-				return;
-			}
+			else
+				exit_flag = true;
 		}
-		if(current % 2 == 0 && current < range)
-			current ++;
-		while(1){
+		if(current % 2 == 0 && !exit_flag){
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
+		while(!exit_flag){
 			for(node* tempnode = primearray;current % tempnode->prime != 0; tempnode = tempnode->next){
 				if(current < tempnode->prime * tempnode->prime){
 					(last)->next = new node(current);
 					last = (last)->next;
-					counter ++;
+					counter++;
 					break;
 				}
 			}
 			if(current +1 < range)
 				current += 2;
-			else{
-				time = clock() - time;
-				print_stats("Sieve of Atkin", counter, time);
-				for(struct node *tempnode; primearray != last;){
-					tempnode = primearray;
-					primearray = primearray->next;
-					delete tempnode;
-				}
-				delete primearray;
-				return;
-			}
+			else
+				exit_flag = true;
 		}
 	}
 	time = clock() - time;
@@ -196,32 +181,39 @@ void miller_rabin(uint64_t current, uint64_t range){
 
 	clock_t time = clock();
 	if(range >= 2){
-		if(current < 2)
+		bool exit_flag = false;
+		if(current <= 2 && !exit_flag)
 			current = 2;
-		while(current < 5){
-			counter += normalizer(current);
+		if(current == 2){
+			counter++;
 			if(current < range)
 				current++;
-			else{
-				time = clock() - time;
-				print_stats("Miller-Rabin", counter, time);
-				return;
-			}
+			else
+				exit_flag = true;
 		}
-		if(current % 2 == 0)
-			current ++;
-		for(; current  <= range; current += 2){
+		while(current < 5 && !exit_flag){
+			for(uint32_t i = 2; current % i != 0; i++){
+				if(i*i > current){
+					counter++;
+					break;
+				}
+			}
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
+		if(current % 2 == 0 && !exit_flag){
+			if(current < range)
+				current++;
+			else
+				exit_flag = true;
+		}
+		for(; current  <= range && !exit_flag; current += 2){
 			if(current == 7 || current == 61){
-				counter ++;
+				counter++;
 				continue;
 			}
-			for(r = 0;((current - 1) >> r) % 2 == 0;r++);
-			d = ((current - 1) >> r);
-			counter += (ml_calc(2, r, d, current) && ml_calc(7, r, d, current) && ml_calc(61, r, d, current));
-		}
-		if(current < range){
-			if(current == 7 || current == 61)
-				counter ++; current += 2;
 			for(r = 0;((current - 1) >> r) % 2 == 0;r++);
 			d = ((current - 1) >> r);
 			counter += (ml_calc(2, r, d, current) && ml_calc(7, r, d, current) && ml_calc(61, r, d, current));
@@ -237,6 +229,7 @@ int main(){
 	miller_rabin(MINIMUM, MAXIMUM);
 	atkin(MINIMUM, MAXIMUM);
 	deterministic(MINIMUM, MAXIMUM);
+	
 
 	printf("\n");
 	return 0;
